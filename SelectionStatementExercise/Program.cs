fcbb9ddb0b1bed519872a42908eef944fe8ef319
Laptop <micahcorbintanner@gmail.com>
2025-02-28 19:30:44 -0700

    Did the assignment

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+        // Loop game forever
+        static void Main(string[] args){
+            bool won = false;
+
+            while (won == false) {
+                won = Game();
+            }
+        }
+
+        static bool Game()
         {
-            Console.WriteLine("Hello, World!");
+            // NO CHEATING
+            // Dont even try to do the math or write code to tell you the awnser.
+            int favriotNumber = (int)(Math.Pow(3, 4) % 4) * 6;
+
+
+            Console.WriteLine("Guess my favorite number:");
+            string? userInput = (Console.ReadLine());
+
+            int userGuess = 0;
+
+            // Both test for non parsable and no entry
+            try {
+                if (userInput == null || userInput == ""){
+                    throw new Exception("No user input");
+                }
+
+                userGuess = int.Parse(userInput);
+            } catch {
+                Console.WriteLine("WRONG");
+                return false;
+            }
+
+            if (userGuess == favriotNumber) {
+                Console.WriteLine("\nWOW your so smart!!!!!\nI hope you didnt cheat....\nI'll find out if you cheated.\n\n\n");
+                return true;
+            } else {
+                Console.WriteLine("WRONG");
+                return false;
+            }
+
         }
     }
 }
